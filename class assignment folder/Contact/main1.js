9a1b1a227c233792f7393b9da58831611ce2c740
spelloner1 <38012986+spelloner1@users.noreply.github.com>
2018-04-24 11:57:49 -0400

    Add files via upload

---
+++ class assignment folder/Contact/main1.js
@@ -0,0 +1,15 @@
+let filterInput=document.querySelector('#filter');
+filterInput.addEventListener('keyup',filterNames);
+function filterNames(){
+	let filterValue=filterInput.value.toUpperCase();
+	let li=document.querySelectorAll('li.sw-link');
+	for(let i=0<li.length;i++){
+		let a=li[i].querySelector('a');
+		if(a.innerHTML.toUpperCase().includes(filterValue)){
+			li[i].style.display='block';
+		}else{
+				li[i].style.display='none';
+
+		}
+	}
+}
