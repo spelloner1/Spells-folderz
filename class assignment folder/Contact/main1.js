6f88dbc69ffdf2d26527bba9d0b30f9f975c05a3
spelloner1 <38012986+spelloner1@users.noreply.github.com>
2018-04-24 15:44:10 -0400

    Add files via upload

--- class assignment folder/Contact/main1.js
+++ class assignment folder/Contact/main1.js
@@ -1,15 +1,24 @@
-let filterInput=document.querySelector('#filter');
+
+
+let filterInput = document.querySelector('#filter');
 filterInput.addEventListener('keyup',filterNames);
 function filterNames(){
-	let filterValue=filterInput.value.toUpperCase();
-	let li=document.querySelectorAll('li.sw-link');
-	for(let i=0<li.length;i++){
-		let a=li[i].querySelector('a');
+	
+	let filterValue = filterInput.value.toUpperCase();
+	let li = document.querySelectorAll('li.sw-link');
+	for(let i = 0;i<li.length;i++){
+		
+		let a = li[i].querySelector('a');
+
+		
 		if(a.innerHTML.toUpperCase().includes(filterValue)){
-			li[i].style.display='block';
-		}else{
-				li[i].style.display='none';
+			li[i].style.display = 'block';
+		}
+
+		else{
+				li[i].style.display = 'none';
 
 		}
 	}
 }
+
