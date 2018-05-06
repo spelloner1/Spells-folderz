55959c78adb9c87fd9c08294913c533e6491d4e4
spelloner1 <38012986+spelloner1@users.noreply.github.com>
2018-05-05 21:15:03 -0400

    Add files via upload

---
+++ class assignment folder/SPELLLZ DEMO 1/main.js
@@ -0,0 +1,128 @@
+
+// number 1
+
+// function printInt(n){
+// 	var i = 1;
+// 	while(i<=n){
+// 		console.log(i);
+// 		i++
+// 	}
+
+// }
+// printInt(5);
+
+
+
+// number 2
+
+// function printInt(n){
+// 	var i = n;
+// 	while(i>=1){
+// 		console.log(i);
+// 		i--
+// 	}
+
+// }
+// printInt(5);
+
+
+ // number 3
+
+// function checkInput(x){
+// 	if(typeof x =="number"){
+// 		return "number";
+
+// 	}else if (typeof x == "string"){
+// 		return "string";
+	
+// 	}else if (typeof x == "boolean"){
+// 		return "true";
+
+// 	}else{
+// 		"return -1";
+// 	}
+
+// }
+// console.log(checkInput());
+
+
+ // number 4
+
+// function SimpleEvenAdding(num){
+// 	var answer = 0;
+// 	for(let i=2;i<=num;i+=2){
+// 		answer= answer+i;
+// 	}
+// 	return answer;
+
+// }
+// console.log(SimpleEvenAdding());
+
+
+// number 5
+
+// function letterCapitalize(str){
+// 	var c=[];
+// 	var sep = str.split(" ");
+// 	for(var i=0;i<sep.length;i++){
+// 	c.push(sep[i][0].toUpperCase()+sep[i].slice(1));
+
+// 	}
+// 	return c.join (" ");
+// }
+// console.log(letterCapitalize("hello world"));
+
+
+// // number 6
+
+// function simpleReverse(str){
+// 	var str="MSIMBO";
+// 	var reverseStr = '';
+// 	for(let i=str.length -1;i>=0;i--)
+// 		reverseStr+=str[i];
+// 	alert("Reversed String=" +reverseStr);
+// }
+// simpleReverse("");
+
+
+// number 7
+
+// function findDiff(arr){
+// 	return Math.max.apply(null,arr)-Math.min.apply(null,arr);
+// }
+
+// console.log(findDiff([ ]));
+
+
+// number 8
+
+// function timeConvert(num){
+// 	return parseInt(num/60) + ":" + (num%60);
+// }
+// console.log(timeConvert());
+
+
+
+// number 9
+
+function findStr(str,long){
+
+var str = "free";
+var long = "free code camp is freestyle and good!";
+count = 0;
+for(let i=0;i<long.length;i++){
+	if(long.charAt(i)==="f"){
+
+	count++;
+}
+
+	findStr("free","fgufuyfuy");
+
+	
+
+		
+	
+	
+	
+	
+
